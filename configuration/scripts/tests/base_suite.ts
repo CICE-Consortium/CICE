0b1b209b1418e28475c357c051b5b11c130aec51
apcraig <anthony.p.craig@gmail.com>
2017-10-24 12:55:25 -0400

    update test suite settings

--- configuration/scripts/tests/base_suite.ts
+++ configuration/scripts/tests/base_suite.ts
@@ -5,7 +5,6 @@ smoke          gx3     4x1        debug,diag1,run5day
 smoke          gx3     8x2        debug,diag1,run5day
 smoke          gx3     4x2        diag1,run5day          smoke_gx3_8x2_diag1_run5day
 smoke          gx3     4x1        diag1,run5day,thread   smoke_gx3_8x2_diag1_run5day
-smoke          gx3     4x1        diag1,run5day          smoke_gx3_4x1_diag1_run5day_thread
 restart        gx3     8x1        none
 restart        gx3     4x2        debug
 restart        gx3     8x2        diag1,pondcesm
@@ -13,4 +12,4 @@ restart        gx3     8x2        diag1,pondtopo
 smoke          gx1    16x1        diag1,run5day,thread
 smoke          gx1     8x2        diag1,run5day          smoke_gx1_16x1_diag1_run5day_thread
 smoke          gx1     8x2        debug,run2day
-restart        gx1     8x1        none
+restart        gx1    13x2        none
