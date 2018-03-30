6be69e45b7dbac1a104993bed6224b945db8368a
apcraig <anthony.p.craig@gmail.com>
2018-03-30 21:06:24 +0000

    update travis suite and add quiet mode to scripts

--- configuration/scripts/tests/travis_suite.ts
+++ configuration/scripts/tests/travis_suite.ts
@@ -1,7 +1,9 @@
 # Test         Grid    PEs        Sets    BFB-compare
-smoke          gx3     1x2        diag1,run5day
 smoke          gx3     2x1        debug,diag1,run5day
 smoke          gx3     1x2        debug,diag1,run5day
+smoke          gx3     1x1        diag1,run5day
+smoke          gx3     2x1        diag1,run5day   smoke_gx3_1x1_diag1_run5day
+smoke          gx3     1x2        diag1,run5day
 smoke          gx3     1x1        diag1,run5day,thread   smoke_gx3_1x2_diag1_run5day
 smoke          gx3     2x1        diag1,run5day,thread   smoke_gx3_1x2_diag1_run5day
 restart        gx3     2x1        diag1
