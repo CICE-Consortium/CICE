19249a824e1d414465c646d39c72077c384d28e4
apcraig <anthony.p.craig@gmail.com>
2018-04-13 03:30:00 +0000

    update travis suite

--- configuration/scripts/tests/travis_suite.ts
+++ configuration/scripts/tests/travis_suite.ts
@@ -1,8 +1,9 @@
 # Test         Grid    PEs        Sets    BFB-compare
-smoke          gx3     2x2        debug,diag1,run1day
-smoke          gx3     1x2        debug,diag1,run1day smoke_gx3_2x2_debug_diag1_run1day
-smoke          gx3     1x1        debug,diag1,run1day,thread smoke_gx3_2x2_debug_diag1_run1day
-smoke          gx3     1x1        diag1,run5day
-smoke          gx3     2x1        diag1,run5day smoke_gx3_1x1_diag1_run5day
-restart        gx3     1x1        diag1
-restart        gx3     2x2        diag1
+smoke          gx3     2x2        run2day
+smoke          gx3     2x1        run2day,thread smoke_gx3_2x2_run2day
+smoke          gx3     1x2        run2day smoke_gx3_2x2_run2day
+smoke          gx3     1x1        run2day,thread smoke_gx3_2x2_run2day
+smoke          gx3     1x1        debug,run2day
+smoke          gx3     2x2        debug,run2day
+restart        gx3     1x1        
+restart        gx3     2x2        
