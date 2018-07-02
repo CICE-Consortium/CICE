d22e775ddbc0a1808e9e63c902b601a9fb93ff30
mhrib <mhri@dmi.dk>
2018-07-02 13:40:53 +0000

    OMP tested ok

--- cicecore/cicedynB/dynamics/evp_kernel1d.F90
+++ cicecore/cicedynB/dynamics/evp_kernel1d.F90
@@ -1723,7 +1723,7 @@ module evp_kernel1d
     !-- Gather data into one single block --
     ! BEGIN: Gather data
     !-- icetmask: gather_global_ext only works for reals
-!MHRI    !$OMP PARALLEL DO PRIVATE(n,i,j)
+    !$OMP PARALLEL DO PRIVATE(n,i,j)
     do n=1,nblk
       do j=1,ny
         do i=1,nx
@@ -1735,9 +1735,9 @@ module evp_kernel1d
         enddo
       enddo
     enddo
-!MHRI    !$OMP END PARALLEL DO
+    !$OMP END PARALLEL DO
     call gather_global_ext(G_work, I_work, master_task, distrb_info)
-!MHRI    !$OMP PARALLEL DO PRIVATE(i,j)
+    !$OMP PARALLEL DO PRIVATE(i,j)
     do j=1,ny_glob
       do i=1,nx_glob
         if (G_work(i,j)>p5)  then
@@ -1747,9 +1747,9 @@ module evp_kernel1d
         endif
       enddo
     enddo
-!MHRI    !$OMP END PARALLEL DO
+    !$OMP END PARALLEL DO
     !-- iceumask: gather_global_ext only works for reals
-!MHRI    !$OMP PARALLEL DO PRIVATE(n,i,j)
+    !$OMP PARALLEL DO PRIVATE(n,i,j)
     do n=1,nblk
       do j=1,ny
         do i=1,nx
@@ -1761,9 +1761,9 @@ module evp_kernel1d
         enddo
       enddo
     enddo
-!MHRI    !$OMP END PARALLEL DO
+    !$OMP END PARALLEL DO
     call gather_global_ext(G_work, I_work, master_task, distrb_info)
-!MHRI    !$OMP PARALLEL DO PRIVATE(i,j)
+    !$OMP PARALLEL DO PRIVATE(i,j)
     do j=1,ny_glob
       do i=1,nx_glob
         if (G_work(i,j)>p5)  then
@@ -1773,7 +1773,7 @@ module evp_kernel1d
         endif
       enddo
     enddo
-!MHRI    !$OMP END PARALLEL DO
+    !$OMP END PARALLEL DO
     !-- the rest
     call gather_global_ext(G_HTE, I_HTE, master_task, distrb_info)
     call gather_global_ext(G_HTN, I_HTN, master_task, distrb_info)
@@ -1903,7 +1903,7 @@ module evp_kernel1d
     G_rdg_conv   = c0
     G_rdg_shear  = c0
     G_shear      = c0
-!MHRI    !$OMP PARALLEL PRIVATE(iw,i,j)
+    !$OMP PARALLEL PRIVATE(iw,i,j)
     do iw=1,NA_len
       i=indi(iw)
       j=indj(iw)
@@ -1930,7 +1930,7 @@ module evp_kernel1d
       G_rdg_shear(i,j)  = rdg_shear(iw)
       G_shear(i,j)      = shear(iw)
     enddo
-!MHRI    !$OMP END PARALLEL
+    !$OMP END PARALLEL
     call dealloc1d()
     !-- Scatter data into blocks --
     ! BEGIN: Scatter data
@@ -2278,14 +2278,17 @@ module evp_kernel1d
 !endif
 
     ! Write 1D data from 2D: Here only extra FD part, the rest follows...
+    !$OMP PARALLEL DO PRIVATE(iw,i,j)
     do iw=na+1,navel
       j=int((indij(iw)-1)/(nx_block))+1
       i=indij(iw)-(j-1)*nx_block
       uvel(iw)=      I_uvel(i,j)
       vvel(iw)=      I_vvel(i,j)
     enddo
+    !$OMP END PARALLEL DO
     
     ! Write 1D data from 2D
+    !$OMP PARALLEL DO PRIVATE(iw,i,j)
     do iw=1,na
       i=indi(iw)
       j=indj(iw)
@@ -2336,6 +2339,7 @@ module evp_kernel1d
       HTEm1(iw) = I_HTE(i-1,j)
       HTNm1(iw) = I_HTN(i,j-1)
     enddo
+    !$OMP END PARALLEL DO
   end subroutine convert_2d_1d
   !=======================================================================
   subroutine union(x,y,nx,ny,xy,nxy)
