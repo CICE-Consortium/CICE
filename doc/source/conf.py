522c33308fc1e4d6bd228338cc05f90567a91c75
Alice DuVivier <duvivier@ucar.edu>
2018-08-28 11:34:27 -0600

    another html_title

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -128,7 +128,7 @@ html_theme_options = {"stickysidebar": "true"}
 
 # The name for this set of Sphinx documents.
 # "<project> v<release> documentation" by default.
-# To manually specify the version, uncomment the following line and specify version.
+# To manually specify the version, uncomment the following line and enter information.
 #html_title = u'CICE XXX'
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
