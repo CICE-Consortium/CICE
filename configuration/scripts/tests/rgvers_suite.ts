a9cbb030a82da4c64254ffeefdd6445fcac6e406
Robert.Grumbine <Robert.Grumbine@noaa.gov>
2018-10-05 20:07:02 +0000

    intel compiler version tests -- 15.0.0 or greater

---
+++ configuration/scripts/tests/rgvers_suite.ts
@@ -0,0 +1,9 @@
+# Test         Grid    PEs        Sets    BFB-compare
+restart        gx3     4x4        none
+restart        gx3     4x4        iobinary
+restart        gx3     8x2        debug
+restart        gx3     6x2        alt01
+restart        gx3     8x2        alt02
+restart        gx3     4x2        alt03
+restart        gx3     4x4        alt04
+restart        gx3     4x4        alt05
