1a1524d45bc8c8604e0fa31e4811654a48b9775c
apcraig <anthony.p.craig@gmail.com>
2019-05-30 22:21:15 +0000

    add noaa machines and minor cleanup from rg

--- configuration/scripts/cice.launch.csh
+++ configuration/scripts/cice.launch.csh
@@ -154,6 +154,33 @@ EOFR
 endif
 
 #=======
+else if (${ICE_MACHINE} =~ theia*) then
+cat >> ${jobfile} << EOFR
+#mpirun -np ${ntasks} ./cice >&! \$ICE_RUNLOG_FILE
+srun -n ${ntasks} -c ${nthrds} ./cice >&! \$ICE_RUNLOG_FILE
+#./cice >&! \$ICE_RUNLOG_FILE
+EOFR
+#=======
+else if (${ICE_MACHINE} =~ high_Sierra*) then
+cat >> ${jobfile} << EOFR
+mpirun -np ${ntasks} ./cice >&! \$ICE_RUNLOG_FILE
+#./cice >&! \$ICE_RUNLOG_FILE
+EOFR
+
+#=======
+else if (${ICE_MACHINE} =~ phase2*) then
+cat >> ${jobfile} << EOFR
+mpirun -np ${ntasks} ./cice >&! \$ICE_RUNLOG_FILE
+#./cice >&! \$ICE_RUNLOG_FILE
+EOFR
+
+#=======
+else if (${ICE_MACHINE} =~ phase3*) then
+cat >> ${jobfile} << EOFR
+mpirun -np ${ntasks} ./cice >&! \$ICE_RUNLOG_FILE
+#./cice >&! \$ICE_RUNLOG_FILE
+EOFR
+#=======
 else if (${ICE_MACHINE} =~ testmachine*) then
 cat >> ${jobfile} << EOFR
 ./cice >&! \$ICE_RUNLOG_FILE
---
+++ configuration/scripts/tests/rgvers_suite.ts
@@ -0,0 +1,18 @@
+# Test         Grid    PEs        Sets    BFB-compare
+#restart        gx1     4x4         droundrobin,short
+#restart        gx1     8x4         droundrobin,short
+#restart        gx1     12x4         droundrobin,short
+#restart        gx1     16x4         droundrobin,short
+#restart        gx1     20x4         droundrobin,short
+#restart        gx1     24x4         droundrobin,short
+#restart        gx1     28x4         droundrobin,short
+#restart        gx1     32x4         droundrobin,short
+restart        gx1     32x2         droundrobin,short
+restart        gx1     32x3         droundrobin,short
+restart        gx1     32x1         droundrobin,short
+
+restart        gx1     33x4         droundrobin,short
+restart        gx1     34x4         droundrobin,short
+restart        gx1     35x4         droundrobin,short
+restart        gx1     36x4         droundrobin,short
+#restart        gx1     40x4       droundrobin,short
