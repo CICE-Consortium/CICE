10fc1ea07665e0bd77f80ee36a9a32ee518dd057
Philippe Blain <philippe.blain@canada.ca>
2019-11-08 16:02:37 -0500

    ice_dyn_vp: fgmres: fix error in the last loop computing the residual
    workspace_[xy] was rewritten at each iteration instead of being
    incremented.
    
    Fix that.

--- cicecore/cicedynB/dynamics/ice_dyn_vp.F90
+++ cicecore/cicedynB/dynamics/ice_dyn_vp.F90
@@ -4256,8 +4256,8 @@
                   i = indxui(ij, iblk)
                   j = indxuj(ij, iblk)
 
-                  workspace_x(i, j, iblk) = rhs_hess(it) * arnoldi_basis_x(i, j, iblk, it)
-                  workspace_y(i, j, iblk) = rhs_hess(it) * arnoldi_basis_y(i, j, iblk, it)
+                  workspace_x(i, j, iblk) = workspace_x(i, j, iblk) + rhs_hess(it) * arnoldi_basis_x(i, j, iblk, it)
+                  workspace_y(i, j, iblk) = workspace_x(i, j, iblk) + rhs_hess(it) * arnoldi_basis_y(i, j, iblk, it)
                enddo ! ij
             enddo
             !$OMP END PARALLEL DO
