e665cf4e515e92c21041387ec02bab121843cc06
david.hebert <david.hebert@nrlssc.navy.mil>
2020-02-26 17:54:55 +0000

    Reverting to version prior to updating ice_open

--- cicecore/cicedynB/infrastructure/ice_read_write.F90
+++ cicecore/cicedynB/infrastructure/ice_read_write.F90
@@ -88,9 +88,6 @@
       integer (kind=int_kind), intent(in), optional :: algn
       integer (kind=int_kind) :: RecSize, Remnant
 
-      ! dah: use nbytes to avoid integer overflow of nx_global*ny_global*nbits
-      integer (kind=int_kind) :: nbytes
-
       character (*) :: filename
 
       character(len=*), parameter :: subname = '(ice_open)'
@@ -102,15 +99,10 @@
             open(nu,file=filename,form='unformatted')
 
          else                   ! direct access
-            !RecSize = nx_global*ny_global*nbits/8  ! dah: original
-            
-            ! dah: use nbytes to avoid integer overflow of nx_global*ny_global*nbits
-            nbytes = nbits/8
-            RecSize = nx_global*ny_global*nbytes
-
+            RecSize = nx_global*ny_global*nbits/8
             if (present(algn)) then
               ! If data is keept in blocks using given sizes (=algn)
-              ! Used in eg. HYCOM binary files, which are stored as "blocks" dividable by 16384 bit (=algn)
+              !  Used in eg. HYCOM binary files, which are stored as "blocks" dividable by 16384 bit (=algn)
               if (algn /= 0) then
                 Remnant = modulo(RecSize,algn)
                 if (Remnant /= 0) then
