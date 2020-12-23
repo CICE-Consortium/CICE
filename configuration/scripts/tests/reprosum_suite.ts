2b10c11bc0e4ce06797626c9a1c071c61154be89
Tony Craig <apcraig@users.noreply.github.com>
2020-12-23 15:06:40 -0800

    Make JRA55 default forcing dataset (#533)
    * Update test suite, add complete set of jra55 tests.
    
    jra55 tests for different grids
      gx3 uses jra55_gx3
      gx1 uses jra55_gx1
      tx1 is already using jra55 by default (no changes to tests required)
      gbox* uses internal forcing, no jra55 data (no changes to tests required)
    
    remove ocn_data_dir='default' and bgc_data_dir='default' in set_nml.jra55*
    files because it causes the bgc tests to fail.  the bgc tests work with WOA bgc
    forcing data with jra55 physics forcing data.
    
    iobinary tests must use the old gx3 binary forcing, the jra55_gx3 netcdf data
    causes an abort because iobinary is tested with netcdf turned off.  the old
    gx3 data is NCAR_bulk in binary format.
    
    alt01 with jra55 uses the alt01a setting.  the difference is that
    use_leap_years is set to false in alt01a.  It's set to true by default
    in the jra55 datasets.
    
    * add wget of JRA55_gx3 forcing to travis yml
    
    * add set_nml.alt01a
    
    * Update options and tests to make JRA55 the default
    
    set_nml.gx3 and set_nml.gx1 are now JRA55 by default
    get rid of set_nml.jra55* files
    add set_nml.gx3ncarbulk and set_nml.gx1coreii which invokes prior forcing datasets
    modify the test suites to do identical tests but with new options, compare
      to prior implementation by creating a renamed regression baseline via
      a script and links to generate the new names.  Use zz* for the new set_nml
      options temporarily just to make it easy to generate the new links
      (the zz ensures the new options are last in the list by alphabet)
    chmod -x all set_* files for consistency
    
    * rename zz test options
    
    this is done as a clean separate step so new baselines can be established to
    continue to evolve the test cases and allow regression testing
    
    * remove most old forcing test cases
    
    * rename alt01a to alt01 in test options and suite

--- configuration/scripts/tests/reprosum_suite.ts
+++ configuration/scripts/tests/reprosum_suite.ts
@@ -4,8 +4,8 @@ logbfb        gx3     4x2x25x29x4   dslenderX2,diag1,reprosum
 sleep 60
 logbfb        gx3     1x1x50x58x4   droundrobin,diag1,thread,maskhalo,reprosum logbfb_gx3_4x2x25x29x4_diag1_dslenderX2_reprosum
 logbfb        gx3     4x1x25x116x1  dslenderX1,diag1,thread,maskhalo,reprosum  logbfb_gx3_4x2x25x29x4_diag1_dslenderX2_reprosum
-logbfb        gx3     1x20x5x29x80  dsectrobin,diag1,short,reprosum   logbfb_gx3_4x2x25x29x4_diag1_dslenderX2_reprosum
-logbfb        gx3     8x2x8x10x20   droundrobin,diag1,reprosum        logbfb_gx3_4x2x25x29x4_diag1_dslenderX2_reprosum
-logbfb        gx3     6x2x50x58x1   droundrobin,diag1,reprosum        logbfb_gx3_4x2x25x29x4_diag1_dslenderX2_reprosum
+logbfb        gx3     1x20x5x29x80  dsectrobin,diag1,short,reprosum            logbfb_gx3_4x2x25x29x4_diag1_dslenderX2_reprosum
+logbfb        gx3     8x2x8x10x20   droundrobin,diag1,reprosum                 logbfb_gx3_4x2x25x29x4_diag1_dslenderX2_reprosum
+logbfb        gx3     6x2x50x58x1   droundrobin,diag1,reprosum                 logbfb_gx3_4x2x25x29x4_diag1_dslenderX2_reprosum
 logbfb        gx3     6x2x4x29x18   dspacecurve,diag1,maskhalo,reprosum        logbfb_gx3_4x2x25x29x4_diag1_dslenderX2_reprosum
-#logbfb        gx3     8x2x8x10x20   droundrobin,diag1                 logbfb_gx3_4x2x25x29x4_diag1_dslenderX2
+#logbfb        gx3     8x2x8x10x20   droundrobin,diag1                          logbfb_gx3_4x2x25x29x4_diag1_dslenderX2
