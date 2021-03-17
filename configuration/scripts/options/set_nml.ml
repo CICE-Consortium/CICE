7b6ed7e941a8c204382e78ba923db785370e6703
David Bailey <dbailey@ucar.edu>
2021-03-17 09:35:37 -0600

    Add ICE_MACHINE_INPUTDATA to set_nml.ml

--- configuration/scripts/options/set_nml.ml
+++ configuration/scripts/options/set_nml.ml
@@ -2,6 +2,6 @@
 oceanmixed_ice = .true.
 ocn_data_type = 'ncar'
 ocn_data_format = 'nc'
-ocn_data_dir    = '/glade/p/cesm/pcwg_dev/CICE_data/forcing/gx1'
+ocn_data_dir    = 'ICE_MACHINE_INPUTDATA/CICE_data/forcing/gx1'
 oceanmixed_file = 'pop_frc.gx1.nc'
 
