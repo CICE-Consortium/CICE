643bc28f1fe8e17addfdb765730ee93e58fd15c5
David Bailey <dbailey@ucar.edu>
2021-03-24 12:17:01 -0600

    Update filename and location for ocean forcing

--- configuration/scripts/options/set_nml.ml
+++ configuration/scripts/options/set_nml.ml
@@ -2,6 +2,6 @@
 oceanmixed_ice = .true.
 ocn_data_type = 'ncar'
 ocn_data_format = 'nc'
-ocn_data_dir    = 'ICE_MACHINE_INPUTDATA/CICE_data/forcing/gx1'
-oceanmixed_file = 'pop_frc.gx1.nc'
+ocn_data_dir    = 'ICE_MACHINE_INPUTDATA/CICE_data/forcing/gx1/CESM/MONTHLY'
+oceanmixed_file = 'ocean_forcing_clim_2D_gx1.nc'
 
