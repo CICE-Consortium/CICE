c0364f4f6924009f9d949d6c0484e832c1c5a9c6
apcraig <anthony.p.craig@gmail.com>
2021-03-27 12:50:44 -0600

    add gx1 debug test, expected to fail

--- configuration/scripts/tests/base_suite.ts
+++ configuration/scripts/tests/base_suite.ts
@@ -39,6 +39,7 @@ restart        gx1     8x1        bgczclim,medium
 smoke          gx1    24x1        medium,run90day,yi2008
 smoke          gx3     8x1        medium,run90day,yi2008
 restart        gx1    24x1        short
+restart        gx1    16x2        seabedLKD,gx1apr,medium,debug
 restart        gx1    16x2        seabedLKD,gx1apr,medium
 restart        gx1    15x2        seabedprob,medium
 restart        gx1    32x1        gx1prod,medium
