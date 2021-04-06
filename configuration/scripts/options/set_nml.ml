afc3a58ea343a22fcaa79435143806514e9d72da
Tony Craig <apcraig@users.noreply.github.com>
2021-04-06 15:16:16 -0700

    Update gx1 initial condition, Icepack, tests, and version.  Fix hmix default value (#586)
    * Update gx1 ic
    
    - update set_nml.gx1prod to match current production system
    - add apr 1 test case for gx1
    - update landice tests, use gx1
    - fix hmix default #585
    - delete old code in ice_forcing.F90 (should have been done in earlier PR)
    
    * update gx1coreii initial condition
    
    * update icepack
    
    * add gx1 debug test, expected to fail
    
    * Update version number to 6.2.0
    
    * Update gx3 and gx1 input filenames
    Update CI wget implementation #588
    Create variable in ice_forcing.F90 called mixed_layer_depth_default and use it instead of c20
    Fix bug in hourly output, created in time manager update #589
    Set start year for all runs to 2005 and turn leap years on by default
    Update documentation in history output section to add information about hist_avg namelist, noted in #566
    
    * update ic filenames, tests, and documentation
    
    * update color links in test results wiki
    
    * update hmix initialization

--- configuration/scripts/options/set_nml.ml
+++ configuration/scripts/options/set_nml.ml
@@ -3,5 +3,5 @@ oceanmixed_ice = .true.
 ocn_data_type = 'ncar'
 ocn_data_format = 'nc'
 ocn_data_dir    = 'ICE_MACHINE_INPUTDATA/CICE_data/forcing/gx1/CESM/MONTHLY'
-oceanmixed_file = 'ocean_forcing_clim_2D_gx1.nc'
+oceanmixed_file = 'ocean_forcing_clim_2D_gx1.20210330.nc'
 
