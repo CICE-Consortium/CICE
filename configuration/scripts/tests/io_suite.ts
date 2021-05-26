97370d740472030386f24471ba5b4ecc6ab1f159
Tony Craig <apcraig@users.noreply.github.com>
2021-05-26 14:30:17 -0700

    Update testing, in particular for diagnostics and decompositions (#602)
    * Update test coverage
    
    - add alt06 test, ncat=7, kcatbound=3, nslyr=3
    - add bigdiag test with lots of diagnostic output, debug_model=true, debug_blocks=true, etc
    - add spiralcenter decomp test
    - add gx1 weightfile decomp test
    - add gbox180 with spacecurve decomp test to trigger Peano and Cinco decompositions
    - rename boxdyn to boxnodyn
    - modify boxslotcyl, turn off tr_lvl and tr_pond_lvl, changes answers
    - clarify boxadv, boxnodyn, boxrestore, explicitly set shortwave=ccsm3, albedo_type=constant
    - cleanup spacecurve implementation including debug flagging, recode print to write, module private,
      add printcurve for gridsize of 30, remove qsort and partition subroutines.
    - remove CICE_RunMod.F90_debug and migrate high level debug checks to CICE_RunMod.F90 with new
      debug_model namelist.  Also add debug_model_step to specify at what timestep to start writing
      debug_model output.  debug_model writes data for the first point specified by lonpnt, latpnt.
    - move debug_ice routine into ice_diagnostics.F90 and out of CICE.F90
    - remove writeout_finished_file from CICE_Finalize in several drivers
    - deprecate print_points_state via an ifdef
    - rename l_stop in ice_transport_driver.F90 to ckflag.  This looked like l_stop logic because
      of the naming but it isn't.
    - fix issue of size of fcontopn_d and fsurfn_d when ncat greater than 6 in init_coupler_flux.  Would
      go out of bounds, now initializes fcontopn and fsurfn to 6th value for any category greater than 6.
    - rename dbug variable name in ice_forcing.F90 to forcing_diag and change namelist variable name.
    - update ice_distributionGet output and implementation to support more general arguments
    - add ice_distributionGet test in init_domain_distribution when debug_blocks is true.
    - fix bug in create_distrb_rake for 1d rake.  add a test that triggers the 1d rake.
    - update debug flag in ice_distribution.F90 leverage debug_blocks.
    - update documentation.
    
    * - Deprecate IsLoadBalanced in ice_spacecurve
    - Update calchk to check 100,000 years
    - Update sectrobin decomp test to check land block elimination
    
    * update complog part of baseline script to report missing data as MISS instead of FAIL

--- configuration/scripts/tests/io_suite.ts
+++ configuration/scripts/tests/io_suite.ts
@@ -7,6 +7,7 @@ restart        gx3    16x2        gx3ncarbulk,alt02,histall,iobinary,precision8
 #restart        gx3     4x2        gx3ncarbulk,alt03,histall,iobinary
 restart        gx3     8x4        gx3ncarbulk,alt04,histall,iobinary,precision8
 restart        gx3     4x4        gx3ncarbulk,alt05,histall,iobinary
+restart        gx3    14x2        gx3ncarbulk,alt06,histall,iobinary,precision8
 restart        gx3    32x1        gx3ncarbulk,bgcz,histall,iobinary,precision8
 restart        gx3    16x2        gx3ncarbulk,bgcskl,histall,iobinary
 restart        gx3    14x2        gx3ncarbulk,isotope,histall,iobinary,precision8
@@ -18,6 +19,7 @@ restart        gx3    15x2        alt02,histall,ionetcdf
 restart        gx3    24x1        alt03,histall,ionetcdf,precision8
 restart        gx3     8x4        alt04,histall,ionetcdf,cdf64
 restart        gx3     8x4        alt05,histall,ionetcdf,precision8,cdf64
+restart        gx3    16x2        alt06,histall,ionetcdf
 restart        gx3    30x1        bgcz,histall,ionetcdf
 restart        gx3    15x2        bgcskl,histall,ionetcdf,precision8
 restart        gx3    31x1        isotope,histall,ionetcdf,cdf64
@@ -29,6 +31,7 @@ restart        gx3    32x1        alt02,histall,iopio1,precision8
 restart        gx3    24x1        alt03,histall,iopio1
 restart        gx3     8x4        alt04,histall,iopio1,precision8,cdf64
 restart        gx3     8x4        alt05,histall,iopio1,cdf64
+restart        gx3    32x1        alt06,histall,iopio1,precision8
 restart        gx3    16x2        bgcz,histall,iopio1,precision8
 restart        gx3    30x1        bgcskl,histall,iopio1
 restart        gx3     8x4        isotope,histall,iopio1,precision8,cdf64
@@ -40,6 +43,7 @@ restart        gx3    32x1        alt02,histall,iopio2,cdf64
 restart        gx3    24x1        alt03,histall,iopio2,precision8
 restart        gx3     8x4        alt04,histall,iopio2
 restart        gx3     8x4        alt05,histall,iopio2,precision8,cdf64
+restart        gx3    16x2        alt06,histall,iopio2,cdf64
 restart        gx3    16x2        bgcz,histall,iopio2,cdf64
 restart        gx3    30x1        bgcskl,histall,iopio2,precision8
 restart        gx3     8x4        isotope,histall,iopio2
@@ -51,6 +55,7 @@ restart        gx3    32x1        alt02,histall,iopio1p,precision8,cdf64
 restart        gx3    24x1        alt03,histall,iopio1p,cdf64
 restart        gx3     8x4        alt04,histall,iopio1p,precision8
 restart        gx3     8x4        alt05,histall,iopio1p
+restart        gx3     6x4        alt06,histall,iopio1p,precision8,cdf64
 restart        gx3    16x2        bgcz,histall,iopio1p,precision8,cdf64
 restart        gx3    30x1        bgcskl,histall,iopio1p,cdf64
 restart        gx3     8x4        isotope,histall,iopio1p,precision8
@@ -62,6 +67,7 @@ restart        gx3    32x1        alt02,histall,iopio2p
 restart        gx3    24x1        alt03,histall,iopio2p,precision8,cdf64
 restart        gx3     8x4        alt04,histall,iopio2p,cdf64
 restart        gx3     8x4        alt05,histall,iopio2p,precision8
+restart        gx3    24x1        alt06,histall,iopio2p
 restart        gx3    16x2        bgcz,histall,iopio2p
 restart        gx3    30x1        bgcskl,histall,iopio2p,precision8,cdf64
 restart        gx3     8x4        isotope,histall,iopio2p,cdf64
