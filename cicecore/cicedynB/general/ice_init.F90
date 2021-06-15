1900861dad77f5b100702a6cd30145adc446bfa8
denise.worthen <Denise.Worthen@noaa.gov>
2021-06-15 11:47:00 -0600

    revert removal of 'latlon' grid_type

--- cicecore/cicedynB/general/ice_init.F90
+++ cicecore/cicedynB/general/ice_init.F90
@@ -1775,6 +1775,7 @@
           grid_type  /=  'rectangular'    .and. &
           grid_type  /=  'cpom_grid'      .and. &
           grid_type  /=  'regional'       .and. &
+          grid_type  /=  'latlon'         .and. &
           grid_type  /=  'setmask' ) then
          if (my_task == master_task) write(nu_diag,*) subname//' ERROR: unknown grid_type=',trim(grid_type)
          abort_list = trim(abort_list)//":20"
