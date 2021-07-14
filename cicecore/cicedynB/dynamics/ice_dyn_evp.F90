e6bdf868a279c60a4986791f7bd66d47ec4726fa
JFLemieux73 <31927797+JFLemieux73@users.noreply.github.com>
2021-07-14 19:15:18 -0400

    New fig for Bgrid + minor modifs to comments and doc (#616)

--- cicecore/cicedynB/dynamics/ice_dyn_evp.F90
+++ cicecore/cicedynB/dynamics/ice_dyn_evp.F90
@@ -610,12 +610,12 @@
          vvel     , & ! y-component of velocity (m/s)
          dxt      , & ! width of T-cell through the middle (m)
          dyt      , & ! height of T-cell through the middle (m)
-         dxhy     , & ! 0.5*(HTE - HTE)
-         dyhx     , & ! 0.5*(HTN - HTN)
-         cyp      , & ! 1.5*HTE - 0.5*HTE
-         cxp      , & ! 1.5*HTN - 0.5*HTN
-         cym      , & ! 0.5*HTE - 1.5*HTE
-         cxm      , & ! 0.5*HTN - 1.5*HTN
+         dxhy     , & ! 0.5*(HTE - HTW)
+         dyhx     , & ! 0.5*(HTN - HTS)
+         cyp      , & ! 1.5*HTE - 0.5*HTW
+         cxp      , & ! 1.5*HTN - 0.5*HTS
+         cym      , & ! 0.5*HTE - 1.5*HTW
+         cxm      , & ! 0.5*HTN - 1.5*HTS
          tarear   , & ! 1/tarea
          tinyarea     ! puny*tarea
 
