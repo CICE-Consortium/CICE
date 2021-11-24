9e6e8f330efbadb7d1613862e32c3b8c5cebfa8b
daveh150 <david.hebert@nrlssc.navy.mil>
2021-11-24 10:58:32 -0600

    Added boxnodyn tests to gridsys_suite.ts. Also added machinery to sub… (#40)
    * Added boxnodyn tests to gridsys_suite.ts. Also added machinery to submit batch jobs on local NRLSSC grid system
    
    * Change NRLSSC to lowercase nrlssc

--- configuration/scripts/tests/gridsys_suite.ts
+++ configuration/scripts/tests/gridsys_suite.ts
@@ -3,6 +3,7 @@ smoke          gx3     8x2        diag1,run5day
 restart        gx3     4x2        debug,diag1
 smoke          gbox80  1x1        box2001
 smoke          gbox80  1x1        boxslotcyl
+smoke          gbox80  2x4        boxnodyn
 smoke	       gbox80  2x2        boxsymn
 smoke	       gbox80  4x2        boxsyme
 smoke	       gbox80  4x1        boxsymne
@@ -13,11 +14,11 @@ smoke	       gbox80  8x1        boxislandsn
 smoke	       gbox80  4x2        boxislandse
 smoke	       gbox80  2x4        boxislandsne
 
-
 smoke          gx3     8x2        diag1,run5day,gridcd
 restart        gx3     4x2        debug,diag1,gridcd
 smoke          gbox80  1x1        box2001,gridcd
 smoke          gbox80  1x1        boxslotcyl,gridcd
+smoke          gbox80  2x4        boxnodyn,gridcd
 smoke	       gbox80  2x2        boxsymn,gridcd
 smoke	       gbox80  4x2        boxsyme,gridcd
 smoke	       gbox80  4x1        boxsymne,gridcd
