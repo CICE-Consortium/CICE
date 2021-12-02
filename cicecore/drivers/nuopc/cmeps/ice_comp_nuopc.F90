545906ec2435bc12d5b055037848827e31a9d563
David Bailey <dbailey@ucar.edu>
2021-12-02 09:33:57 -0700

    Put dragio in the namelist instead

--- cicecore/drivers/nuopc/cmeps/ice_comp_nuopc.F90
+++ cicecore/drivers/nuopc/cmeps/ice_comp_nuopc.F90
@@ -376,8 +376,7 @@ contains
        depressT_in         = 0.054_dbl_kind,                  &
        Tocnfrz_in          = -34.0_dbl_kind*0.054_dbl_kind,   &
        pi_in               = SHR_CONST_PI,                    &
-       snowpatch_in        = 0.005_dbl_kind,                  &
-       dragio_in           = 0.00536_dbl_kind)
+       snowpatch_in        = 0.005_dbl_kind)
 
     call icepack_warnings_flush(nu_diag)
     if (icepack_warnings_aborted()) call abort_ice(error_message=subname, &
