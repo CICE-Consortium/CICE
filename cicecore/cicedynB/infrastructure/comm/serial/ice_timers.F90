667e998c9ac61bf66eb781659e0385fd693aecf9
apcraig <anthony.p.craig@gmail.com>
2022-01-12 10:53:05 -0700

    Comment out temporary timers

--- cicecore/cicedynB/infrastructure/comm/serial/ice_timers.F90
+++ cicecore/cicedynB/infrastructure/comm/serial/ice_timers.F90
@@ -58,16 +58,17 @@
       timer_forcing,          &! forcing
       timer_evp_1d,           &! timer only loop
       timer_evp_2d,           &! timer including conversion 1d/2d
-      timer_updstate,         &! update state
-      timer_tmp1,             &! for temporary timings
-      timer_tmp2,             &! for temporary timings
-      timer_tmp3,             &! for temporary timings
-      timer_tmp4,             &! for temporary timings
-      timer_tmp5,             &! for temporary timings
-      timer_tmp6,             &! for temporary timings
-      timer_tmp7,             &! for temporary timings
-      timer_tmp8,             &! for temporary timings
-      timer_tmp9               ! for temporary timings
+      timer_updstate           ! update state
+!      timer_updstate,         &! update state
+!      timer_tmp1,             &! for temporary timings
+!      timer_tmp2,             &! for temporary timings
+!      timer_tmp3,             &! for temporary timings
+!      timer_tmp4,             &! for temporary timings
+!      timer_tmp5,             &! for temporary timings
+!      timer_tmp6,             &! for temporary timings
+!      timer_tmp7,             &! for temporary timings
+!      timer_tmp8,             &! for temporary timings
+!      timer_tmp9               ! for temporary timings
 
 !-----------------------------------------------------------------------
 !
@@ -210,15 +211,15 @@
    call get_ice_timer(timer_evp_1d,   '1d-evp',   nblocks,distrb_info%nprocs)
    call get_ice_timer(timer_evp_2d,   '2d-evp',   nblocks,distrb_info%nprocs)
    call get_ice_timer(timer_updstate, 'UpdState', nblocks,distrb_info%nprocs)
-   call get_ice_timer(timer_tmp1,     'tmp1',     nblocks,distrb_info%nprocs)
-   call get_ice_timer(timer_tmp2,     'tmp2',     nblocks,distrb_info%nprocs)
-   call get_ice_timer(timer_tmp3,     'tmp3',     nblocks,distrb_info%nprocs)
-   call get_ice_timer(timer_tmp4,     'tmp4',     nblocks,distrb_info%nprocs)
-   call get_ice_timer(timer_tmp5,     'tmp5',     nblocks,distrb_info%nprocs)
-   call get_ice_timer(timer_tmp6,     'tmp6',     nblocks,distrb_info%nprocs)
-   call get_ice_timer(timer_tmp7,     'tmp7',     nblocks,distrb_info%nprocs)
-   call get_ice_timer(timer_tmp8,     'tmp8',     nblocks,distrb_info%nprocs)
-   call get_ice_timer(timer_tmp9,     'tmp9',     nblocks,distrb_info%nprocs)
+!   call get_ice_timer(timer_tmp1,     'tmp1',     nblocks,distrb_info%nprocs)
+!   call get_ice_timer(timer_tmp2,     'tmp2',     nblocks,distrb_info%nprocs)
+!   call get_ice_timer(timer_tmp3,     'tmp3',     nblocks,distrb_info%nprocs)
+!   call get_ice_timer(timer_tmp4,     'tmp4',     nblocks,distrb_info%nprocs)
+!   call get_ice_timer(timer_tmp5,     'tmp5',     nblocks,distrb_info%nprocs)
+!   call get_ice_timer(timer_tmp6,     'tmp6',     nblocks,distrb_info%nprocs)
+!   call get_ice_timer(timer_tmp7,     'tmp7',     nblocks,distrb_info%nprocs)
+!   call get_ice_timer(timer_tmp8,     'tmp8',     nblocks,distrb_info%nprocs)
+!   call get_ice_timer(timer_tmp9,     'tmp9',     nblocks,distrb_info%nprocs)
 
 !-----------------------------------------------------------------------
 
