b7d88d957345ff7e7506c157a1811c6f887fb19d
denise.worthen <Denise.Worthen@noaa.gov>
2023-07-07 07:38:31 -0500

    tidy up the wrapper
    * fix case for 'time since' at the first advance

--- cicecore/drivers/nuopc/cmeps/cice_wrapper_mod.F90
+++ cicecore/drivers/nuopc/cmeps/cice_wrapper_mod.F90
@@ -1,35 +1,50 @@
 module cice_wrapper_mod
 
 #ifdef CESMCOUPLED
-  use perf_mod     , only : t_startf, t_stopf, t_barrierf
-  use shr_file_mod , only : shr_file_getlogunit, shr_file_setlogunit
+  use perf_mod      , only : t_startf, t_stopf, t_barrierf
+  use shr_file_mod  , only : shr_file_getlogunit, shr_file_setlogunit
 
+  use ice_kinds_mod , only : dbl_kind, int_kind, char_len, char_len_long
+
+  implicit none
+contains
+  ! Define stub routines that do nothing - they are just here to avoid
+  ! having cppdefs in the main program
+  subroutine ufs_settimer(timevalue)
+    real(dbl_kind),    intent(out)   :: timevalue
+  end subroutine ufs_settimer
+  subroutine ufs_logtimer(nunit,etime,string,time0)
+    integer,           intent(in)    :: nunit
+    integer(int_kind), intent(in)    :: etime
+    character(len=*),  intent(in)    :: string
+    real(dbl_kind),    intent(in)    :: time0
+  end subroutine ufs_logtimer
 #else
   use ice_kinds_mod , only : dbl_kind, int_kind, char_len, char_len_long
 
   implicit none
 
-  real(dbl_kind) :: timeiads = 0.0, timeirls = 0.0, timeadv = 0.0, timefs = 0.0
-#endif
+  real(dbl_kind)                     :: timeiads = 0.0, timeirls = 0.0, timeadv = 0.0, timefs = 0.0
 contains
-  ! Define stub routines that do nothing - they are just here to avoid
-  ! having cppdefs in the main program
-#ifndef CESMCOUPLED
   subroutine ufs_settimer(timevalue)
-    real(dbl_kind), intent(inout) :: timevalue
-    real(dbl_kind)                :: MPI_Wtime
+    real(dbl_kind),    intent(inout) :: timevalue
+    real(dbl_kind)                   :: MPI_Wtime
     timevalue = MPI_Wtime()
   end subroutine ufs_settimer
   subroutine ufs_logtimer(nunit,etime,string,time0)
-    integer,          intent(in)  :: nunit
-    integer(int_kind), intent(in) :: etime
-    character(len=*), intent(in)  :: string
-    real(dbl_kind),   intent(in)  :: time0
-    real(dbl_kind)                :: MPI_Wtime, timevalue
-    timevalue = MPI_Wtime()-time0
-    write(nunit,*)etime,' CICE '//trim(string),timevalue
+    integer,           intent(in)    :: nunit
+    integer(int_kind), intent(in)    :: etime
+    character(len=*),  intent(in)    :: string
+    real(dbl_kind),    intent(in)    :: time0
+    real(dbl_kind)                   :: MPI_Wtime, timevalue
+    if (time0 > 0.) then
+       timevalue = MPI_Wtime()-time0
+       write(nunit,*)etime,' CICE '//trim(string),timevalue
+    end if
   end subroutine ufs_logtimer
 
+  ! Define stub routines that do nothing - they are just here to avoid
+  ! having cppdefs in the main program
   subroutine shr_file_setLogUnit(nunit)
     integer, intent(in) :: nunit
   end subroutine shr_file_setLogUnit
@@ -47,16 +62,6 @@ contains
     character(len=*) :: string
     integer:: comm
   end subroutine t_barrierf
-#else
-  subroutine ufs_settimer(timevalue)
-    real(dbl_kind), intent(out) :: timevalue
-  end subroutine ufs_settimer
-  subroutine ufs_logtimer(nunit,etime,string,time0)
-    character(len=*), intent(in)  :: string
-    integer(int_kind), intent(in) :: etime
-    real(dbl_kind),   intent(in)  :: time0
-    integer,          intent(in)  :: nunit
-  end subroutine ufs_logtimer
 #endif
 
 end module cice_wrapper_mod
