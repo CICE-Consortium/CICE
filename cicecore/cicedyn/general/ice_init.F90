eb1463a8734efd828500b05649cecd00e762558a
sanAkel <santha.akella@gmail.com>
2025-09-20 18:51:40 +0000

    Addition 2 of 3 from awallcraft:SSMI from https://github.com/awallcraft/CICE-EMC/tree/SSMI

--- cicecore/cicedyn/general/ice_init.F90
+++ cicecore/cicedyn/general/ice_init.F90
@@ -84,7 +84,7 @@
           restart, restart_ext, restart_coszen, use_restart_time, &
           runtype, restart_file, restart_dir, runid, pointer_file, &
           restart_format, restart_rearranger, restart_iotasks, restart_root, &
-          restart_stride, restart_deflate, restart_chunksize
+          restart_stride, restart_deflate, restart_chunksize, insert_sic
       use ice_history_shared, only: &
           history_precision, hist_avg, history_format, history_file, incond_file, &
           history_dir, incond_dir, version_name, history_rearranger, &
@@ -294,7 +294,7 @@
         atm_data_dir,   ocn_data_dir,    bgc_data_dir,                  &
         atm_data_format, ocn_data_format, rotate_wind,                  &
         oceanmixed_file, atm_data_version,semi_implicit_Tsfc,           &
-        vapor_flux_correction
+        vapor_flux_correction, insert_sic
 
       !-----------------------------------------------------------------
       ! default values
@@ -573,6 +573,7 @@
       restore_ocn     = .false.   ! restore sst if true
       trestore        = 90        ! restoring timescale, days (0 instantaneous)
       restore_ice     = .false.   ! restore ice state on grid edges if true
+      insert_sic      = .false.   ! if true, on restart update concentration from a file
       debug_forcing   = .false.   ! true writes diagnostics for input forcing
 
       latpnt(1) =  90._dbl_kind   ! latitude of diagnostic point 1 (deg)
@@ -1170,6 +1171,7 @@
       call broadcast_scalar(restore_ocn,          master_task)
       call broadcast_scalar(trestore,             master_task)
       call broadcast_scalar(restore_ice,          master_task)
+      call broadcast_scalar(insert_ice,           master_task)
       call broadcast_scalar(debug_forcing,        master_task)
       call broadcast_array (latpnt(1:2),          master_task)
       call broadcast_array (lonpnt(1:2),          master_task)
@@ -2683,6 +2685,7 @@
          write(nu_diag,1011) ' restore_ice      = ', restore_ice
          if (restore_ice .or. restore_ocn) &
          write(nu_diag,1021) ' trestore         = ', trestore
+         write(nu_diag,1011) ' insert_sic       = ', insert_sic
 
          write(nu_diag,*) ' '
          write(nu_diag,'(a31,2f8.2)') 'Diagnostic point 1: lat, lon =', &
