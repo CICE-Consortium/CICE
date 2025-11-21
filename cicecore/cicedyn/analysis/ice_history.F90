be9b930ddb91a8a1f130e7afdacb88c869dadc42
apcraig <anthony.p.craig@gmail.com>
2025-11-21 12:46:59 -0700

    Add initial history restart feature in io_netcdf

--- cicecore/cicedyn/analysis/ice_history.F90
+++ cicecore/cicedyn/analysis/ice_history.F90
@@ -2204,7 +2204,7 @@
       use ice_domain, only: blocks_ice, nblocks
       use ice_domain_size, only: nfsd
       use ice_grid, only: tmask, lmask_n, lmask_s, dxU, dyU, grid_ice
-      use ice_calendar, only: new_year, write_history, &
+      use ice_calendar, only: new_year, write_history, write_restart, &
                               write_ic, timesecs, histfreq, nstreams, mmonth, &
                               new_month
       use ice_dyn_eap, only: a11, a12, e11, e12, e22, s11, s12, s22, &
@@ -4036,6 +4036,27 @@
          endif                  ! 1st of July
       enddo                     ! iblk
       !$OMP END PARALLEL DO
+
+      !---------------------------------------------------------------
+      ! write history restarts
+      !---------------------------------------------------------------
+
+      if (write_restart) then
+        ! turn on histrest features
+        write_histrest = .true.
+
+        ! write history restarts
+        call ice_timer_start(timer_readwrite)  ! reading/writing
+        do ns = 1, nstrm
+           if (hist_avg(ns)) then  ! only write avg history file
+              call ice_write_hist (ns)
+           endif
+        enddo
+        call ice_timer_stop(timer_readwrite)  ! reading/writing
+
+        ! turn off histrest features
+        write_histrest = .false.
+      endif
 
       end subroutine accum_hist
 
